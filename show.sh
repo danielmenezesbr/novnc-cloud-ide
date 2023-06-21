02b8dd9ae0254a68d27ddef8fbbed070f58e2f35
danielmenezesbr <danielmenezes@gmail.com>
2023-06-21 17:40:47 +0000

    improve access to noVNC

--- show.sh
+++ show.sh
@@ -2,13 +2,14 @@ set +xu
 EXTERNAL_URL="http://localhost:6901"
 if [[ ! -z "${GITPOD_WORKSPACE_URL}" ]]; then
   EXTERNAL_URL=${GITPOD_WORKSPACE_URL}
-  EXTERNAL_URL=${EXTERNAL_URL/https:\/\//https:\/\/6901-}
+  EXTERNAL_URL=${EXTERNAL_URL/https:\/\//https:\/\/6901-}/vnc.html?password=headless
 fi
 if [[ ! -z "${GOOGLE_CLOUD_SHELL}" ]]; then
-  EXTERNAL_URL="https://shell.cloud.google.com/devshell/proxy?authuser=0&port=6901&environment_id=default"
+  #EXTERNAL_URL="https://shell.cloud.google.com/devshell/proxy?authuser=0&port=6901&environment_id=default"
+  EXTERNAL_URL="https://6901-${WEB_HOST}/vnc.html?password=headless"
 fi
 if [[ ! -z "${CODESPACES}" ]]; then
-  EXTERNAL_URL="https://${CODESPACE_NAME}-6901.preview.app.github.dev"
+  EXTERNAL_URL="https://${CODESPACE_NAME}-6901.preview.app.github.dev/vnc.html?password=headless"
 fi
 printf "\n\n"
 echo "noVNC: $EXTERNAL_URL"
